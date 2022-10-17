f699d26cc5bba327c21f96b08b44e7f2785ce3a6
Evan <77792892+Evan-sudo@users.noreply.github.com>
2022-10-17 11:35:47 +0800

    Update stMRF.m

--- stMRF.m
+++ stMRF.m
@@ -1,8 +1,8 @@
-function [ refine_D, w, b ] = stMRF( V, D_t, uD_t, lambda_t, r, eps)
+function [ refine_D, w, b ] = stMRF( V, D_t, uD_t, lambda_t, r, eps, mex)
 
-uV = convBox(V, r);
-uVV = convBox(V.*V, r);
-uVD_t=convBox(bsxfun(@times,V,D_t), r);
+uV = convBox(V, r, 1, mex);
+uVV = convBox(V.*V, r, 1, mex);
+uVD_t=convBox(bsxfun(@times,V,D_t), r, 1, mex);
 
 numerator = sum(bsxfun(@times, uVD_t-bsxfun(@times,uV,uD_t),lambda_t),3);
 denominator = uVV - uV.^2;
